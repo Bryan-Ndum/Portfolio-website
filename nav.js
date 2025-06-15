f1481ae3cd725f6c4da95a010f23c91cf7788b38
Bryan Ndum <ndumbaba@gmail.com>
2025-06-15 01:05:35 -0400

    Delete nav.js

--- nav.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-  const menu = document.getElementById('navLinks');
-  menu.classList.toggle('active');
-}
-
-function closeMenu() {
-  const menu = document.getElementById('navLinks');
-  menu.classList.remove('active');
-}
